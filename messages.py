f2af23f0adbb735f00235a99b054d723241df21e
arthursl12 <arthursoutolima@gmail.com>
2021-03-09 15:35:12 -0300

    Mensagem Chunk_info

--- messages.py
+++ messages.py
@@ -1,5 +1,11 @@
 from common import listByteDecoder, listByteEncoder, msgId, messageList
 
+"""
+Mensagens e seus códigos:
+Hello       1
+Get         4
+Chunk_info  3
+"""
 def hello_encode(lst):
     """
     Cria e retorna uma mensagem do tipo Hello: 
@@ -29,24 +35,35 @@ def get_encode(lst):
 
 def get_decode(msg):
     """
-    Decodifica uma mensagem em binário do tipo Hello.
+    Decodifica uma mensagem em binário do tipo Get.
     Verifica seu id e tamanho, levanta uma exceção se houver algo errado
     Retorna a lista contida na mensagem
     """
     assert msgId(msg) == 4
     return listByteDecoder(msg[2:])
 
+def chunk_info_encode(lst):
+    """
+    Cria e retorna uma mensagem do tipo Chunk_info: 
+        2 bytes, representando o 3 (código da mensagem)
+        2 bytes, representando quantas chunks na lista lst
+        2 bytes * len(lst), representando a lista em bytearray
+    """
+    return messageList(3,lst)
+
+def chunk_info_decode(msg):
+    """
+    Decodifica uma mensagem em binário do tipo Chunk_info.
+    Verifica seu id e tamanho, levanta uma exceção se houver algo errado
+    Retorna a lista contida na mensagem
+    """
+    assert msgId(msg) == 3
+    return listByteDecoder(msg[2:])
 
 def query_encode():
     pass
 
 def query_decode():
-    pass
-
-def chunk_info_encode():
-    pass
-
-def chunk_info_decode():
     pass
 
 def response_encode():
--- test_messages.py
+++ test_messages.py
@@ -8,7 +8,7 @@ class TestId:
     def test_msgId(self):
         assert msgId(messages.hello_encode([1,2,3])) == 1
         assert msgId(messages.get_encode([1])) == 4
-        assert msgId(messages.info_file_encode("teste1.txt",135)) == 3
+        assert msgId(messages.chunk_info_encode([2,5,3,67,3])) == 3
         assert msgId(messages.ok_encode()) == 4
         assert msgId(messages.fim_encode()) == 5
         assert msgId(messages.ack_encode(0)) == 7
@@ -73,6 +73,37 @@ class TestGetEncode:
         assert lst == [3]
         
         lst = messages.get_decode(messages.get_encode([]))
+        assert lst == []
+
+class TestChunkInfoEncode:
+    def test_hello_msg_varios(self):
+        ba = bytearray()
+        ba.extend((3).to_bytes(length=2, byteorder='big'))
+        ba.extend((3).to_bytes(length=2, byteorder='big'))
+        ba.extend(listToBytes([3,1,2]))
+        assert ba == messages.chunk_info_encode([3,1,2])
+    
+    def test_hello_msg_unico(self):
+        ba = bytearray()
+        ba.extend((3).to_bytes(length=2, byteorder='big'))
+        ba.extend((1).to_bytes(length=2, byteorder='big'))
+        ba.extend(listToBytes([3]))
+        assert ba == messages.chunk_info_encode([3])
+    
+    def test_hello_msg_vazio(self):
+        ba = bytearray()
+        ba.extend((3).to_bytes(length=2, byteorder='big'))
+        ba.extend((0).to_bytes(length=2, byteorder='big'))
+        assert ba == messages.chunk_info_encode([])
+    
+    def test_hello_decode(self):
+        lst = messages.chunk_info_decode(messages.chunk_info_encode([3,1,2]))
+        assert lst == [3,1,2]
+        
+        lst = messages.chunk_info_decode(messages.chunk_info_encode([3]))
+        assert lst == [3]
+        
+        lst = messages.chunk_info_decode(messages.chunk_info_encode([]))
         assert lst == []
     
 # class TestOk:
