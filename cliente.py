7e7bcb58fa7339c528d2bc377a15c4b1fba08b43
arthursl12 <arthursoutolima@gmail.com>
2021-03-10 19:00:32 -0300

    Parse argumentos do cliente

--- cliente.py
+++ cliente.py
@@ -1,5 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+import socket
+
+# cliente <IP:port> <5,6,7>
+
 def main():
-    args = argParse()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ipporta", help="IP:porta do ponto de contat (IPv4)")
+    parser.add_argument("chunks", help="Lista de Chunks a serem baixadas")
+    args = parser.parse_args()
+    
+    print(f"IP:porta = {args.ipporta}")
+    # Trata o IP e a porta
+    ipPortaLst = args.ipporta.split(":")
+    assert len(ipPortaLst) == 2
+    ip = socket.inet_ntoa(socket.inet_aton(ipPortaLst[0]))  
+    porta = int(ipPortaLst[1])
+    print(f"IP={ip}, porta={porta}")
+
+    # Trata a lista de chunks
+    chunksStr = ("".join(args.chunks)).split(",")
+    chunks = []
+    for i in chunksStr:
+        chunks.append(int(i))
+    print(chunks)
+    
+    
     udp_socket = connectUDP(args.ip, infoServer[0])
 
     
